1501217f6d67913287352d589b6211e8653fce1e
Dayana Osorio <dayrox2301@gmail.com>
2017-10-17 16:24:42 +0200

    doing things

---
+++ cambios_crm/ventana_modal_reasignar.php
@@ -0,0 +1,25 @@
+<div class="ui small reasignar modal">
+  <i class="close icon"></i>
+  <div class="header">
+    Información Importante
+  </div>
+  <div class="image content">
+    <div class="ui  image">
+      <i class="ui orange warning sign icon"></i>
+    </div>
+    <div class="description">
+      <div class="ui header">ATENCIÓN.</div>
+      <p>Debe Tomar en cuenta que al ACEPTAR esta reasignación, la tarea pasara al panel de control de la Clinica elegida.</p>
+      <p>Esta seguro de Reasignar esta Tarea?</p>
+    </div>
+  </div>
+  <div class="actions">
+    <div class="ui black deny button">
+      No
+    </div>
+    <div class="ui positive right labeled icon button">
+      Si, Estoy De acuerdo
+      <i class="checkmark icon"></i>
+    </div>
+  </div>
+</div>
